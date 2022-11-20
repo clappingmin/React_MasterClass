58af0fafce60d118518a77e2836ab659046a2f20
Sumin Park <clappingmin@gmail.com>
2022-11-20 20:27:03 +0900

    #2.7 Themes

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -2,49 +2,21 @@ import styled, { keyframes } from 'styled-components';
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  } 
-  50% {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100% {  
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
 
-  ${Emoji} {
-    &:hover {
-      font-size: 100px;
-    }
-  }
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
-    <Wrapper as="header">
-      <Box>
-        <Emoji>🤩</Emoji>
-      </Box>
+    <Wrapper>
+      <Title>안녕하세요</Title>
     </Wrapper>
   );
 }
