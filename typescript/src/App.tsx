637b3460ea1ae79c3137da4fd290f0ab184ef181
Sumin Park <clappingmin@gmail.com>
2022-11-24 23:27:53 +0900

    #3.5 Forms

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import Circle from './Circle';
+import React, { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState('');
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('hello', value);
+  };
+
   return (
     <div>
-      <Circle bgColor="tomato" />
-      <Circle bgColor="teal" borderColor="pink" text="Im sumin" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
