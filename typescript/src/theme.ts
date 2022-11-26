137f9e6920faf9fd7543cd0cf9953e5eea1e8cb1
Sumin Park <clappingmin@gmail.com>
2022-11-26 22:25:16 +0900

    #3.6 Themes

---
+++ typescript/src/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from './styled.d';
+
+const lightTheme: DefaultTheme = {
+  bgColor: 'white',
+  textColor: 'black',
+};
+
+const darkTheme: DefaultTheme = {
+  bgColor: 'black',
+  textColor: 'white',
+};
